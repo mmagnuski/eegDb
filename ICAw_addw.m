10cefb737ea213204d02fc2327fc24493abffcdf
mmagnuski <mmagnuski@swps.edu.pl>
2014-08-19 21:32:40 +0200

    Some folder work. Folder names are temporary. Related to #6

--- ICAw_addw.m
+++
@@ -1,9 +0,0 @@
-function ICAw = ICAw_addw(ICAw, r, EEG)
-
-% NOHELPINFO
-% NOTUSED
-
-        ICAw(r).icaweights = EEG.icaweights;
-        ICAw(r).icasphere = EEG.icasphere;
-        ICAw(r).icawinv = EEG.icawinv;
-        ICAw(r).icachansind = EEG.icachansind;
