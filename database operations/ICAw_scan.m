d0fd08d1327cd83280833e20e68aa003de589130
mmagnuski <mmagnuski@swps.edu.pl>
2014-10-23 19:41:10 +0200

    adding proto of ICAw_set

---
+++ database operations/ICAw_scan.m
@@ -0,0 +1,95 @@
+function info = ICAw_get(ICAw, rs)
+
+% NOHELPINFO
+% Helper function for getting info about properties 
+% of database entries - it is mostly used for checking 
+% safty of chnges that user wants to introduce
+
+% returns vector of information where:
+%  0 - given option is unspecified
+%  1 - given option is specified but not saved in file (only introduced when recovering)
+%  2 - option is specified and saved in EEG file (present in .datainfo of the database)
+% -1 - option present both in .datainfo and core entry - may be an error
+
+if ~exist('rs', 'var')
+    rs = 1:length(ICAw);
+end
+
+numR = length(rs);
+info = zeros(5, numR);
+
+
+% FILTER
+info = simple_testfield(ICAw(rs), 'filter', [], info, 1);
+
+% CLEANLINE
+istrue = @(x) isequal(x, true);
+info = simple_testfield(ICAw(rs), 'cleanline', [], info, 2, istrue);
+
+% EPOCHING
+% just checking if the field is empty may not be enough...
+info = simple_testfield(ICAw(rs), 'epoch', [], info, 3);
+
+% rem epochs
+% pre
+info = simple_testfield(ICAw(rs), 'reject', 'pre', info, 4);
+% post
+info = simple_testfield(ICAw(rs), 'reject', 'post', info, 5);
+
+% ICA
+info = simple_testfield(ICAw(rs), 'ICA', 'icaweights', info, 6);
+
+% removed components
+info = simple_testfield(ICAw(rs), 'ICA', 'remove', info, 7);
+
+
+function vec = simple_testfield(ICAw, fname, subf, vec, n, varargin)
+    if isempty(subf)
+        isf = isfield(ICAw, fname);
+        if isf
+            f = ~cellfun(@isempty, {ICAw.(fname)});
+        else
+            f = false(1, length(ICAw));
+        end
+        d = cellfun(@(x) femp(x, fname), {ICAw.datainfo});
+
+        if ~isempty(varargin)
+            for v = 1:length(varargin)
+                if isf
+                    f(f) = cellfun(varargin{v}, {ICAw(f).(fname)});
+                end
+                d(d) = cellfun(@(x) feval(varargin{v}, x.(fname)), {ICAw(d).datainfo});
+            end
+        end
+    else
+        isf = isfield(ICAw, fname);
+        if isf
+            getf = {ICAw.(fname)};
+            f = cellfun(@(x) femp(x, subf), getf);
+        else
+            getf = [];
+            f = false(1, length(ICAw));
+        end
+
+        d    = cellfun(@(x) femp(x, fname), {ICAw.datainfo});
+        d(d) = cellfun(@(x) femp(x.fname, subf), {ICAw(d).datainfo});
+
+        if ~isempty(varargin)
+            for v = 1:length(varargin)
+                if isf
+                    f(f) = cellfun(@(x) feval(varargin{v}, x.(subf)), getf(f));
+                end
+                d(d) = cellfun(@(x) feval(varargin{v}, x.(fname).(subf)), {ICAw(d).datainfo});
+            end
+        end
+    end
+
+    % apply changes
+    vec = fillvec(vec, f, d, n);
+
+function vec = fillvec(vec, f, d, n)
+    vec(n,~f & ~d) = 0;
+    vec(n, f & ~d) = 1;
+    vec(n,~f &  d) = 2;
+    vec(n, f &  d) = -1;
+
---
+++ database operations/ICAw_set.m
@@ -0,0 +1,56 @@
+function ICAw_set(ICAw, fld, val, rs)
+
+% ICAw_set allows for introducing controlled (safe) changes to 
+% the ICAw structure
+%
+% ICAw_set(ICAw, fld, val)
+
+% check matrix:
+%  0 - no conflict possible
+%  1 - no conflict
+% -1 - simple conflict (direct change)
+% -2 - dependency conflict
+%  2 - leave vs remove
+%  3 - depends on choice in 2 - leave, compare, remove
+%  4 - compare vs remove
+ch = [-1,  0,  0,  0,  0,  0;...  % filter
+	  -2, -1,  0,  0,  0,  0;...  % cleanline
+	   1,  1, -1,  0,  0,  0;...  % epoch
+	   1,  1,  4, -1,  0,  0;...  % pre reject
+	   1,  1,  4,  4, -1,  0;...  % post reject
+	   2,  2,  2,  2,  2, -1;...  % ICA
+	   3,  3,  3,  3,  3,  2 ];   % remove components
+
+% at the present one cannot change pre-rejection without
+% changing epoching - this will change as pre-rejection
+% could be not including epochs with eye act within some
+% time from the stim pres or with RT shorter or longer
+% than some threshold (or some other field-comparison)
+
+all_flds = {'filter', 'cleanline', 'epoch', ...
+	'pre reject', 'post reject', 'ICA', ...
+	'ICA remove',...
+	};
+
+% check chosen change
+f = find(strcmp(fld, all_flds));
+
+if isempty(f)
+	% ADD - description of allowed operations
+	error('change operation not recognized!');
+end
+
+if ~exist('rs', 'var')
+    rs = 1:length(ICAw);
+end
+
+% first scan records that will be changed
+scan = ICAw_scan(ICAw, rs);
+
+ispres = scan > 0;
+
+% relevant row from ch:
+ch = ch(:,f);
+
+% multip element-wise with ispres:
+prob = ch .* ispres;
