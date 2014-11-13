39d0852a0079aa1986f594e8a68f473876f2e244
mmagnuski <mmagnuski@swps.edu.pl>
2014-11-13 21:10:17 +0100

    function for running ICA on exposed db versions

---
+++ database operations/ICAw_runica.m
@@ -0,0 +1,27 @@
+function ICAw = ICAw_runica(ICAw, rs)
+
+% NOHLEPINFO
+
+if ~exist('rs', 'var')
+	rs = 1:length(ICAw);
+end
+
+for r = rs
+    if isempty(ICAw(r).ICA.icaweights)
+        
+        % recover
+        EEG = recoverEEG(ICAw, r, 'local');
+        
+        % select good channels:
+        allchan = 1:size(EEG.data,1);
+        allchan(ICAw(r).chan.bad) = [];
+        
+        % perform ICA
+        EEG = pop_runica(EEG, 'extended', 1, 'interupt',...
+            'off', 'verbose', 'on', 'chanind', allchan);
+        
+        % apply weights
+        ICAw = ICAw_addw(ICAw, r, EEG);
+        
+    end    
+end
