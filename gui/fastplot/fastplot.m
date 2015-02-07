0c9df862a3fe28056d50cecd47f53ef824747a1a
mmagnuski <mmagnuski@swps.edu.pl>
2015-02-07 14:06:40 +0100

    changed limits, ylimmode and figure/axis size

--- gui/fastplot/fastplot.m
+++ gui/fastplot/fastplot.m
@@ -3,6 +3,7 @@ classdef fastplot < handle
     % currently - one epoch at time
     properties
         h
+        opt
         spacing
         data
         data_size
@@ -11,7 +12,8 @@ classdef fastplot < handle
         win_step
     end
     
-    
+    % graphics performance
+    % http://www.mathworks.com/help/matlab/graphics-performance.html
     
     methods
         
@@ -25,30 +27,42 @@ classdef fastplot < handle
             % calculate spacing (
             chan_sd = std(obj.data, [], 1);
             obj.spacing = 2 * max(chan_sd);
-            obj.data = obj.data + repmat(...
+            obj.data = obj.data - repmat(...
                 (0:obj.data_size(2)-1)*obj.spacing, [obj.data_size(1), 1]);
             
             % window limits and step size
             obj.win_lims = [1, 1000];
             obj.win_span = obj.win_lims(1):obj.win_lims(2);
             obj.win_step = 1000;
+            
+            % get screen resolution:
+            obj.opt.scrsz = get(0, 'ScreenSize');
+            obj.opt.scrsz = obj.opt.scrsz([3, 4]);
+            
+            % launch the plot
+            obj.launchplot();
         end
         
         
-        function launchplot(obj)
+        function launchplot(obj) % should be a private method
             % figure setup
-            obj.h.fig = figure();
-            obj.h.ax = axes();
+            ss = obj.opt.scrsz;
+            obj.h.fig = figure('Position', [10, 50, ss(1)-20, ss(2)-200]);
+            obj.h.ax = axes('Position', [0.05, 0.05, 0.9, 0.85]);
             
             % plot data
             % ---------
             % CHANGE!
             % use 'ColorOrder' to set color of electrodes
             obj.h.lines = plot(obj.data(obj.win_span, :));
-        end
             
+            % set y limits and y lim mode (for faster replotting)
+            set(obj.h.ax, 'YLim', [-(obj.data_size(2)+1) * obj.spacing,...
+                obj.spacing], 'YLimMode', 'manual');
+        end
         
-        function refresh(obj, mthd)
+        
+        function refresh(obj, mthd) % should check if sth actually changed
             % during re-plotting:
             % always use set 'XData', 'YData'
             obj.win_span = obj.win_lims(1):obj.win_lims(2);
