e09de041297d17e6af13eba72f2b674f849113c6
mmagnuski <mmagnuski@swps.edu.pl>
2015-02-21 19:18:12 +0100

    FIX - add warning to compsel compare if some comps rmvd before

--- gui/compsel/compsel_compare_changes.m
+++ gui/compsel/compsel_compare_changes.m
@@ -1,14 +1,36 @@
 function compsel_compare_changes(h, evnt)
 
 % compare signal with and without rejected components
-
+ 
 if strcmp(evnt.Key, 'c')
     info = getappdata(h, 'info');
-    remcmp = find(info.comps.state == 1);
+    % get original component numbers
+    remcmp = info.comps.all(info.comps.state == 1);
+    warn = false;
+    % if some components already deleted, check remcmp:
+    if isfield(info, 'mapping')
+    	isinEEG = arrayfun(@(x) any(info.mapping == x), remcmp);
+    	remcmp = arrayfun(@(x) find(info.mapping == x), remcmp(isinEEG));
+    	if ~all(isinEEG)
+    		warn = true;
+    		warntext = sprintf(['%i of %i components selected ', ...
+                'for rejection have already been rejected from ', ...
+                'the current data, so you are comparing only a ', ...
+                'subset of the changes.'], sum(isinEEG == 0), ...
+                length(isinEEG));
+    	end
+    end
     
     if ~isempty(remcmp)
         EEG = getappdata(h, 'EEG');
         EEG2 = pop_subcomp(EEG, remcmp, 0);
         fst = fastplot(EEG, EEG2);
+        if warn
+        	mh = msgbox(warntext, 'Some components have already been removed', ...
+        		'warn');
+            ah = get( h, 'CurrentAxes' );
+            ch = get( ah, 'Children' );
+            set( ch, 'FontSize', 14 );
+        end
     end
 end
