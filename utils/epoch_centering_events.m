c32c007d3dff674f5cd6723261fbaf74f6395248
mmagnuski <mmagnuski@swps.edu.pl>
2015-04-27 23:55:24 +0200

    clean up epoch centering events

--- utils/epoch_centering_events.m
+++ utils/epoch_centering_events.m
@@ -1,26 +1,37 @@
-function [isCenter, eventNames] = epoch_centering_events(EEG, eventNames)
+function [isCenter, whichCenter, eventNames] = epoch_centering_events(EEG, eventNames)
 
 % NOHELPINFO
 
 if ~exist('eventNames', 'var')
-	eventNames = unique({EEG.event.type});
+    eventNames = unique({EEG.event.type});
 end
 
 % go through epochs and find epoch-centering event
 epN = length(EEG.epoch);
 isCenter = false(length(eventNames), epN);
+whichCenter = zeros(1, epN);
 
 for e = 1:epN
-	times = cell2mat(EEG.epoch(e).eventlatency);
-	zero_event = times == 0;
-	zero_event_name = EEG.epoch(e).eventtype(zero_event);
+    % eventlatency sometimes is cell and sometimes mat...
+    if iscell(EEG.epoch(e).eventlatency)
+        times = cell2mat(EEG.epoch(e).eventlatency);
+    else
+        times = EEG.epoch(e).eventlatency;
+    end
 
-	for evtp = zero_event_name
-		nms = strcmp(evtp{1}, eventNames);
+    zero_event = times == 0;
 
-		if any(nms)
-			isCenter(nms, e) = true;
-		end
+    if any(zero_event)
+        whichCenter(e) = find(zero_event);
+        zero_event_name = EEG.epoch(e).eventtype(zero_event);
 
-	end
+        for evtp = zero_event_name
+            nms = strcmp(evtp{1}, eventNames);
+
+            if any(nms)
+                isCenter(nms, e) = true;
+            end
+
+        end
+    end
 end
