181203ece47b1adc27387146ca69168388250ab5
mmagnuski <mmagnuski@swps.edu.pl>
2015-05-29 10:48:58 +0200

    adds simple fun for getting ic signal

---
+++ utils/get_ica_data.m
@@ -0,0 +1,17 @@
+function data = get_ica_data(EEG, ind)
+
+% returns EEG.icaact or calculates
+% it if it is not present
+
+if ~exist('ind', 'var')
+    ind = 1:size(EEG.icaweights, 1);
+end
+
+if femp(EEG, 'icaact')
+	data = EEG.icaact;
+else
+	dt_size = size(EEG.data);
+	dt_size(1) = length(ind);
+    data = reshape((EEG.icaweights(ind, :) * EEG.icasphere) ...
+    	* data(EEG.icachansind, :), dt_size);
+end
