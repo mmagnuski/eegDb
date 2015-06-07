987ebcd189f1dde451c2e6f264c52e214074233e
mmagnuski <mmagnuski@swps.edu.pl>
2015-06-07 10:38:43 +0200

    remove unnecessary files

--- tests/profile_scanmarks.m
+++
@@ -1,8 +0,0 @@
-% timeit!
-
-tic
-for i = 1:1000
-    rejt = ICAw_scanmarks(ICAw2);
-end
-toc
-% Elapsed time is 3.351338 seconds.
--- tests/selcomps_manual_tests.m
+++
@@ -1,9 +0,0 @@
-% tests that support development of
-% pop_selectcomps_new
-
-% setup
-proj_DiamDec;
-EEG = recoverEEG(ICAw, 10, 'local');
-
-% first plot test :)
-pop_selectcomps_new(EEG, 1:20, 'perfig', 10);
