946073924b3161c26f03405ac738d53700b44534
mmagnuski <mmagnuski@swps.edu.pl>
2015-06-20 17:35:41 +0200

    add docstring to get_valid_path

--- db/paths/get_valid_path.m
+++ db/paths/get_valid_path.m
@@ -1,6 +1,37 @@
 function pth = get_valid_path(PTH, varargin)
 
-% FIXHELPINFO
+% GET_VALID_PATH checks which path is valid on a given machine
+%
+% usage
+% -----
+% pth = get_valid_path(PTH, ...);
+%
+% input
+% -----
+% PTH - *string* or *cell array of strings*; path string(s)
+%
+% ... - additional key - value pairs
+%
+% key-value pairs
+% ---------------
+% 'file'    - *string*; filename that should be present
+%             in the valid path
+% 'noerror' - *logical*; whether NOT to throw an error when valid
+%             path was not found
+%             default: false (error is thrown)
+%
+% examples
+% --------
+% check which path to Dropbox is correct on given machine:
+% PTH = {'D:\Dropbox\', 'C:\Users\Kant\Dropbox\'};
+% validpath = get_valid_path(PTH);
+%
+% check which folder exists AND has file 'data.set' inside:
+% PTH = {'C:\DATA\superstudy\', 'C:\DATA\otherstudy\'};
+% validpath = get_valid_path(PTH, 'file', 'data.set');
+%
+% see also: isdir
+
 % additional args
 opt.file = [];
 opt.noerror = false;
