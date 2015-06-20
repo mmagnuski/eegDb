ae11869758668ca812375a8c4563a66910fd4567
mmagnuski <mmagnuski@swps.edu.pl>
2015-06-20 17:35:12 +0200

    get_valid_path now can look for presence of a file

--- db/paths/get_valid_path.m
+++ db/paths/get_valid_path.m
@@ -1,6 +1,13 @@
-function pth = get_valid_path(PTH)
+function pth = get_valid_path(PTH, varargin)
 
 % FIXHELPINFO
+% additional args
+opt.file = [];
+opt.noerror = false;
+if nargin > 1
+    opt = parse_arse(varargin, opt);
+end
+iffile = ~isempty(opt.file);
 
 % if not cell - close in a cell
 if ~iscell(PTH)
@@ -12,12 +19,23 @@ fnd = false;
 for p = 1:length(PTH)
     % and test isdir() on them
     if isdir(PTH{p})
+        % if filecheck, check if file is present:
+        if iffile
+            fls = dir(PTH{p});
+            fileok = any(strcmp(opt.file, {fls(~[fls.isdir]).name}));
+        end
+
         % if it is dir, stop looking
-        pth = PTH{p};
-        fnd = true;
-        break
+        if ~iffile || fileok
+            pth = PTH{p};
+            fnd = true;
+            break
+        end
     end
 end
-if ~fnd
-    error('Could not find the correct path');
+
+if ~opt.noerror
+    if ~fnd
+        error('Could not find the correct path');
+    end
 end
