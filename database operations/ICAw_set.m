697198fc10003a636d45d0114badfd3e2e55912a
mmagnuski <mmagnuski@swps.edu.pl>
2015-08-13 22:33:15 +0200

    break up ICAw_set to two funs - one db_check_conflict

--- database operations/ICAw_set.m
+++ database operations/ICAw_set.m
@@ -5,52 +5,14 @@ function ICAw_set(ICAw, fld, val, rs)
 %
 % ICAw_set(ICAw, fld, val)
 
-% check matrix:
-%  0 - no conflict possible
-%  1 - no conflict
-% -1 - simple conflict (direct change)
-% -2 - dependency conflict
-%  2 - leave vs remove
-%  3 - depends on choice in 2 - leave, compare, remove
-%  4 - compare vs remove
-ch = [-1,  0,  0,  0,  0,  0;...  % filter
-	  -2, -1,  0,  0,  0,  0;...  % cleanline
-	   1,  1, -1,  0,  0,  0;...  % epoch
-	   1,  1,  4, -1,  0,  0;...  % pre reject
-	   1,  1,  4,  4, -1,  0;...  % post reject
-	   2,  2,  2,  2,  2, -1;...  % ICA
-	   3,  3,  3,  3,  3,  2 ];   % remove components
+conflict_matrix = db_check_conflict(db, rs, field);
 
-% at the present one cannot change pre-rejection without
-% changing epoching - this will change as pre-rejection
-% could be not including epochs with eye act within some
-% time from the stim pres or with RT shorter or longer
-% than some threshold (or some other field-comparison)
+% different GUI options for different conflicts
 
-all_flds = {'filter', 'cleanline', 'epoch', ...
-	'pre reject', 'post reject', 'ICA', ...
-	'ICA remove',...
-	};
-
-% check chosen change
-f = find(strcmp(fld, all_flds));
-
-if isempty(f)
-	% ADD - description of allowed operations
-	error('change operation not recognized!');
-end
-
-if ~exist('rs', 'var')
-    rs = 1:length(ICAw);
-end
-
-% first scan records that will be changed
-scan = ICAw_scan(ICAw, rs);
-
-ispres = scan > 0;
-
-% relevant row from ch:
-ch = ch(:,f);
-
-% multip element-wise with ispres:
-prob = repmat(ch, [1, size(ispres,2)]) .* ispres;
+% current "recompute and compare" options would be:
+% - for rejections --> moving them from the previous
+%   state of the database to the current by an operation
+%   like:
+%    > compare % or sample coverage and mark
+% - for ICA --> comparing previous components with current
+%   (if they exist) and moving component labels.
