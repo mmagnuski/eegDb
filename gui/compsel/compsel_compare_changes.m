a522b839b3da4f772c66a69a70cb32f415ace59d
unknown <mmagnus1@SWA-01201W.uczelnia.local>
2016-02-21 18:20:34 +0100

    WIP - window property is read-only, need to add method to modify or sth

--- gui/compsel/compsel_compare_changes.m
+++ gui/compsel/compsel_compare_changes.m
@@ -25,6 +25,20 @@ if strcmp(evnt.Key, 'c')
         EEG = getappdata(h, 'EEG');
         EEG2 = pop_subcomp(EEG, remcmp, 0);
         fst = fastplot(EEG, EEG2);
+        
+        % TODO:
+        % 1. if options present - apply
+        fst_opts = getappdata(h, 'fastplotopts');
+        if femp(fst_opts, 'window')
+            fst.window = fst_opts.window;
+            fst.opt.num_epoch_per_window = fst_opts.num_epochs;
+            fst.opt.signal_scale = fst_opts.scale;
+            fst.refresh();
+        end
+        % 2. attach a function to fastplot close action
+        %    (or add a listener to the close event)
+        set(fst.h.fig, 'CloseRequestFcn', @(o, e) store_fastplotopts(fst, h));
+        
         if warn
         	mh = msgbox(warntext, 'Some components have already been removed', ...
         		'warn');
