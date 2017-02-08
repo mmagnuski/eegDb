5a56411cc95203ea059556eb0c240b1f47030aac
mmagnuski <mmagnuski@swps.edu.pl>
2017-02-08 21:32:26 +0100

    some new changes

--- db/versions/db_mainversion.m
+++ db/versions/db_mainversion.m
@@ -1,21 +1,20 @@
 function db = db_mainversion(db, rr)
 
-% NOHELPINFO
+% update main version
 
 % if no - create
 % else - give back(?)
 
 for r = rr
-f = db_checkfields(db, r, [],...
-    'ignore', {'subjectcode', 'tasktype', 'filename', 'filepath',...
-    'datainfo', 'session', 'versions'});
-fld = f.fields(f.fnonempt);
-        db(r).versions.current = 'main';
-        db(r).versions.main.version_name = 'main';
-        db(r).versions.main.version_description = 'main version';
-        
-        for f = 1:length(fld)
-            db(r).versions.main.(fld{f}) = db(r).(fld{f});
-        end
+    f = db_checkfields(db, r, [],...
+        'ignore', {'subjectcode', 'tasktype', 'filename', 'filepath',...
+        'datainfo', 'session', 'versions'});
+    fld = f.fields(f.fnonempt);
+    db(r).versions.current = 'main';
+    db(r).versions.main.version_name = 'main';
+    db(r).versions.main.version_description = 'main version';
+    
+    for f = 1:length(fld)
+        db(r).versions.main.(fld{f}) = db(r).(fld{f});
+    end
 end
-        
